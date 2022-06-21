0edce0cd4620cd19f61dd2fc7a48224b9fd74cd2
Vadim Rogov <rogov.2099@yandex.com>
2022-06-21 15:12:24 +0300

    add tests

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['utils', 'components'],
+};
---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,5 @@
+describe('test', () => {
+  it('should be equal 4', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
