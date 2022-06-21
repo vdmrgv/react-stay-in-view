01dc498a7903b45f0389fc9acb620b1ce8a321a6
Vadim Rogov <rogov.2099@yandex.com>
2022-06-21 15:12:09 +0300

    add typescript

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-stay-in-view
+
+A hook or component to keep the element from disappearing from the screen on scroll.
--- src/actions.js
+++
@@ -1,8 +0,0 @@
-import actions from "./action-types";
-
-export function showOrHide(value) {
-  return {
-    type: actions.SHOW_OR_HIDE_LOGO,
-    payload: value
-  };
-}
