e2d04fd4cd1f961a0ebafd1dd44123e8a59c2c59
Vadim Rogov <rogov.2099@yandex.com>
2022-07-08 20:41:28 +0300

    update  lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,32 +7,32 @@ export const getAvoidOverlapPlacement = (elementRect: DOMRect, anchorRect: DOMRe
   const shouldReverseLeftPlacement = anchorRect.left - elementRect.width < 0;
 
   switch (placement) {
-  case ElementPlacement.TOP_START:
-    return shouldReverseTopPlacement ? ElementPlacement.BOTTOM_START : ElementPlacement.TOP_START;
-  case ElementPlacement.TOP:
-    return shouldReverseTopPlacement ? ElementPlacement.BOTTOM : ElementPlacement.TOP;
-  case ElementPlacement.TOP_END:
-    return shouldReverseTopPlacement ? ElementPlacement.BOTTOM_END : ElementPlacement.TOP_END;
-  case ElementPlacement.RIGHT_START:
-    return shouldReverseRightPlacement ? ElementPlacement.LEFT_START : ElementPlacement.RIGHT_START;
-  case ElementPlacement.RIGHT:
-    return shouldReverseRightPlacement ? ElementPlacement.LEFT : ElementPlacement.RIGHT;
-  case ElementPlacement.RIGHT_END:
-    return shouldReverseRightPlacement ? ElementPlacement.LEFT_END : ElementPlacement.RIGHT_END;
-  case ElementPlacement.BOTTOM_START:
-    return shouldReverseBottomPlacement ? ElementPlacement.TOP_START : ElementPlacement.BOTTOM_START;
-  case ElementPlacement.BOTTOM:
-    return shouldReverseBottomPlacement ? ElementPlacement.TOP : ElementPlacement.BOTTOM;
-  case ElementPlacement.BOTTOM_END:
-    return shouldReverseBottomPlacement ? ElementPlacement.TOP_END : ElementPlacement.BOTTOM_END;
-  case ElementPlacement.LEFT_START:
-    return shouldReverseLeftPlacement ? ElementPlacement.RIGHT_START : ElementPlacement.LEFT_START;
-  case ElementPlacement.LEFT:
-    return shouldReverseLeftPlacement ? ElementPlacement.RIGHT : ElementPlacement.LEFT;
-  case ElementPlacement.LEFT_END:
-    return shouldReverseLeftPlacement ? ElementPlacement.RIGHT_END : ElementPlacement.LEFT_END;
-  default:
-    return placement;
+    case ElementPlacement.TOP_START:
+      return shouldReverseTopPlacement ? ElementPlacement.BOTTOM_START : ElementPlacement.TOP_START;
+    case ElementPlacement.TOP:
+      return shouldReverseTopPlacement ? ElementPlacement.BOTTOM : ElementPlacement.TOP;
+    case ElementPlacement.TOP_END:
+      return shouldReverseTopPlacement ? ElementPlacement.BOTTOM_END : ElementPlacement.TOP_END;
+    case ElementPlacement.RIGHT_START:
+      return shouldReverseRightPlacement ? ElementPlacement.LEFT_START : ElementPlacement.RIGHT_START;
+    case ElementPlacement.RIGHT:
+      return shouldReverseRightPlacement ? ElementPlacement.LEFT : ElementPlacement.RIGHT;
+    case ElementPlacement.RIGHT_END:
+      return shouldReverseRightPlacement ? ElementPlacement.LEFT_END : ElementPlacement.RIGHT_END;
+    case ElementPlacement.BOTTOM_START:
+      return shouldReverseBottomPlacement ? ElementPlacement.TOP_START : ElementPlacement.BOTTOM_START;
+    case ElementPlacement.BOTTOM:
+      return shouldReverseBottomPlacement ? ElementPlacement.TOP : ElementPlacement.BOTTOM;
+    case ElementPlacement.BOTTOM_END:
+      return shouldReverseBottomPlacement ? ElementPlacement.TOP_END : ElementPlacement.BOTTOM_END;
+    case ElementPlacement.LEFT_START:
+      return shouldReverseLeftPlacement ? ElementPlacement.RIGHT_START : ElementPlacement.LEFT_START;
+    case ElementPlacement.LEFT:
+      return shouldReverseLeftPlacement ? ElementPlacement.RIGHT : ElementPlacement.LEFT;
+    case ElementPlacement.LEFT_END:
+      return shouldReverseLeftPlacement ? ElementPlacement.RIGHT_END : ElementPlacement.LEFT_END;
+    default:
+      return placement;
   }
 };
 
@@ -63,54 +63,54 @@ export const getPosition = (
     avoidAnchorOverlap && anchorIsVisible ? getAvoidOverlapPlacement(elementRect, anchorRect, placement) : placement;
 
   switch (avoidPlacement) {
-  case ElementPlacement.TOP_START:
-    position.x = anchorRect.left;
-    position.y = anchorRect.top - elementRect.height;
-    break;
-  case ElementPlacement.TOP:
-    position.x = anchorRect.left - elementRect.width / 2 + anchorRect.width / 2;
-    position.y = anchorRect.top - elementRect.height;
-    break;
-  case ElementPlacement.TOP_END:
-    position.x = anchorRect.left + anchorRect.width - elementRect.width;
-    position.y = anchorRect.top - elementRect.height;
-    break;
-  case ElementPlacement.RIGHT_START:
-    position.x = anchorRect.left + anchorRect.width;
-    position.y = anchorRect.top;
-    break;
-  case ElementPlacement.RIGHT:
-    position.x = anchorRect.left + anchorRect.width;
-    position.y = anchorRect.top - elementRect.height / 2 + anchorRect.height / 2;
-    break;
-  case ElementPlacement.RIGHT_END:
-    position.x = anchorRect.left + anchorRect.width;
-    position.y = anchorRect.top - elementRect.height + anchorRect.height;
-    break;
-  case ElementPlacement.BOTTOM_START:
-    position.x = anchorRect.left;
-    position.y = anchorRect.top + anchorRect.height;
-    break;
-  case ElementPlacement.BOTTOM:
-    position.x = anchorRect.left + anchorRect.width / 2 - elementRect.width / 2;
-    position.y = anchorRect.top + anchorRect.height;
-    break;
-  case ElementPlacement.BOTTOM_END:
-    position.x = anchorRect.left + anchorRect.width - elementRect.width;
-    position.y = anchorRect.top + anchorRect.height;
-    break;
-  case ElementPlacement.LEFT_START:
-    position.x = anchorRect.left - elementRect.width;
-    position.y = anchorRect.top;
-    break;
-  case ElementPlacement.LEFT:
-    position.x = anchorRect.left - elementRect.width;
-    position.y = anchorRect.top - elementRect.height / 2 + anchorRect.height / 2;
-    break;
-  case ElementPlacement.LEFT_END:
-    position.x = anchorRect.left - elementRect.width;
-    position.y = anchorRect.top - elementRect.height + anchorRect.height;
-    break;
+    case ElementPlacement.TOP_START:
+      position.x = anchorRect.left;
+      position.y = anchorRect.top - elementRect.height;
+      break;
+    case ElementPlacement.TOP:
+      position.x = anchorRect.left - elementRect.width / 2 + anchorRect.width / 2;
+      position.y = anchorRect.top - elementRect.height;
+      break;
+    case ElementPlacement.TOP_END:
+      position.x = anchorRect.left + anchorRect.width - elementRect.width;
+      position.y = anchorRect.top - elementRect.height;
+      break;
+    case ElementPlacement.RIGHT_START:
+      position.x = anchorRect.left + anchorRect.width;
+      position.y = anchorRect.top;
+      break;
+    case ElementPlacement.RIGHT:
+      position.x = anchorRect.left + anchorRect.width;
+      position.y = anchorRect.top - elementRect.height / 2 + anchorRect.height / 2;
+      break;
+    case ElementPlacement.RIGHT_END:
+      position.x = anchorRect.left + anchorRect.width;
+      position.y = anchorRect.top - elementRect.height + anchorRect.height;
+      break;
+    case ElementPlacement.BOTTOM_START:
+      position.x = anchorRect.left;
+      position.y = anchorRect.top + anchorRect.height;
+      break;
+    case ElementPlacement.BOTTOM:
+      position.x = anchorRect.left + anchorRect.width / 2 - elementRect.width / 2;
+      position.y = anchorRect.top + anchorRect.height;
+      break;
+    case ElementPlacement.BOTTOM_END:
+      position.x = anchorRect.left + anchorRect.width - elementRect.width;
+      position.y = anchorRect.top + anchorRect.height;
+      break;
+    case ElementPlacement.LEFT_START:
+      position.x = anchorRect.left - elementRect.width;
+      position.y = anchorRect.top;
+      break;
+    case ElementPlacement.LEFT:
+      position.x = anchorRect.left - elementRect.width;
+      position.y = anchorRect.top - elementRect.height / 2 + anchorRect.height / 2;
+      break;
+    case ElementPlacement.LEFT_END:
+      position.x = anchorRect.left - elementRect.width;
+      position.y = anchorRect.top - elementRect.height + anchorRect.height;
+      break;
   }
 
   return {
