d13bec4ee2d6738427ba285df78023c575f0551b
Vadim Rogov <54108395+vdmrgv@users.noreply.github.com>
2022-07-19 12:43:52 +0300

    fix - sourcemap warning

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
+import { babel } from '@rollup/plugin-babel';
 import pkg from './package.json';
-
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: pkg.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: pkg.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
@@ -26,15 +26,19 @@ export default [
       external(),
       resolve(),
       commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: ['**/__tests__', '**/*.test.ts', 'node_modules/**'],
+      }),
       typescript({
-        tsconfig: "./tsconfig.json",
+        tsconfig: './tsconfig.json',
       }),
       terser(),
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
 ];
