1365808061edacdb5251e3bc8bb65dbb508439a3
Vadim Rogov <54108395+vdmrgv@users.noreply.github.com>
2023-02-15 13:12:05 +0300

    build - upgrade dependencies - remove source map generation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
-import { babel } from '@rollup/plugin-babel';
 import pkg from './package.json' assert { type: 'json' };
 
 export default [
@@ -14,30 +14,32 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: false,
       },
       {
         file: pkg.module,
         format: 'esm',
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: [
       external(),
       resolve(),
-      commonjs(),
       babel({
         babelHelpers: 'bundled',
         exclude: ['**/__tests__', '**/*.test.ts', 'node_modules/**'],
       }),
+      commonjs(),
       typescript({
         tsconfig: './tsconfig.json',
+        exclude: ['**/__tests__', '**/*.test.ts'],
+        sourceMap: true,
       }),
       terser(),
     ],
   },
   {
-    input: 'dist/esm/index.d.ts',
+    input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
